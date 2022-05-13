9aaf72a1daa23eb33f3217e96f2497a2a98340d5
luke.sims@bupadentalcare.co.uk <lukesims12@gmail.com>
2022-05-13 15:12:25 +0100

    🐛 Colour bug fix.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,7 +2,6 @@ import { Fab, IconButton, Popover } from "@mui/material";
 import { grey, red } from "@mui/material/colors";
 import { FunctionComponent, useState } from "react";
 import "react-color-palette/lib/css/styles.css";
-import ColourSelector from "./ColourSelector";
 import Navbar from "./Navbar";
 import Pomodoro from "./Pomodoro";
 import PaletteIcon from '@mui/icons-material/Palette';
@@ -10,11 +9,16 @@ import { ColorPicker, useColor } from "react-color-palette";
 
 const Home: FunctionComponent = () => {
 
+    const [selectedColour, setSelectedColour] = useState<string>(red[600]);
+
+    const handleColourChange = (selectedColour: string) => setSelectedColour(selectedColour);
     const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+    
     const open = Boolean(anchorEl);
     const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => setAnchorEl(event.currentTarget);
     const handleClose = () => setAnchorEl(null);
     const [colour, setColour] = useColor("hex", red[500]);
+    
 
     return (
         <div style={{ background: colour.hex, height: '100vh' }}>
@@ -22,7 +26,7 @@ const Home: FunctionComponent = () => {
             <Pomodoro colour={colour.hex} />
             {/* <ToDoList /> */}
             <Fab variant="extended" onClick={handleClick} sx={{'&:hover': {background: grey[900]}, backgroundColor: 'rgba(0,0,0,.2)', position: 'absolute', bottom: '2em', right: '2em', color: '#FFF', display: 'flex', alignItems: 'center'}}>
-                <PaletteIcon sx={{color: '#FFF'}} />&emsp;Colour Picker
+                <PaletteIcon sx={{color: '#FFF'}} />&nbsp;Colour Picker
             </Fab>   
             <Popover
                 open={open}
