c53dcc82a57191c8f2b28d2e89bc91fee83334ab
luke.sims@bupadentalcare.co.uk <luke.sims@bupadentalcare.co.uk>
2022-06-13 15:56:19 +0100

    🔥 Remove WIP sign.

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -21,18 +21,10 @@ const ToDoList: FunctionComponent = () => {
         <div style={{ width: '100vw', marginTop: '1em', display: 'flex', flexDirection: 'row', justifyContent: 'center', gap: '4em' }}>
             <div>
                 <div style={{ color: '#FFF' }}>Tasks: {todos.length}</div>
-                <Badge badgeContent={"WIP"} sx={{
-                    "& .MuiBadge-badge": {
-                        color: "#FFF",
-                        backgroundColor: "#FFC107"
-                    }
-                }}
-                >
-                    <Paper elevation={12} sx={{ padding: '.5em .75em', width: '30em', display: 'flex', alignItems: 'center', gap: '1em', backgroundColor: 'rgba(0,0,0,.2)' }}>
-                        <TextField onChange={(e) => setTask(e.target.value)} InputLabelProps={{ style: { color: '#FFF' } }} label="Add Task..." variant="standard" sx={{ width: '100%', input: { color: '#FFF' }, '& .MuiInput-underline:before': { borderBottomColor: '#FFF' }, '& .MuiInput-underline:after': { borderBottomColor: '#FFF' }, '& .MuiInput-underline:hover': { borderBottomColor: '#FFF' } }} />
-                        <Button onClick={addToDo} variant='contained' sx={{ '&:hover': { background: grey[800] }, background: grey[900] }}>ADD</Button>
-                    </Paper>
-                </Badge>
+                <Paper elevation={12} sx={{ padding: '.5em .75em', width: '30em', display: 'flex', alignItems: 'center', gap: '1em', backgroundColor: 'rgba(0,0,0,.2)' }}>
+                    <TextField onChange={(e) => setTask(e.target.value)} InputLabelProps={{ style: { color: '#FFF' } }} label="Add Task..." variant="standard" sx={{ width: '100%', input: { color: '#FFF' }, '& .MuiInput-underline:before': { borderBottomColor: '#FFF' }, '& .MuiInput-underline:after': { borderBottomColor: '#FFF' }, '& .MuiInput-underline:hover': { borderBottomColor: '#FFF' } }} />
+                    <Button onClick={addToDo} variant='contained' sx={{ '&:hover': { background: grey[800] }, background: grey[900] }}>ADD</Button>
+                </Paper>
             </div>
             <div>
                 <div style={{ color: '#FFF', borderRadius: '.25em', width: '30em' }}>
@@ -41,7 +33,7 @@ const ToDoList: FunctionComponent = () => {
                             Tasks will appear here...
                         </div>
                     }
-                    <div style={{height: '50vh', overflowY: todos.length >= 8 ? 'scroll' : 'hidden'}}>{todos && todos?.map((e: IToDo, index: number) => (
+                    <div style={{ height: '50vh', overflowY: todos.length >= 8 ? 'scroll' : 'hidden' }}>{todos && todos?.map((e: IToDo, index: number) => (
                         <ToDoItem key={index} index={index} completeToDo={completeToDo} todo={e} />
                     ))}</div>
                 </div>
